d58feafb78f1d90bda24f8699180a8f00733c247
Kagendoooo <kagendoooo@gmail.com>
2023-10-11 00:18:18 +0300

    returns a list of lists of integers representing the Pascal’s triangle of n

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -12,8 +12,8 @@ def pascal_triangle(n):
     while len(triangles) != n:
         t = triangles[-1]
         tmp = [1]
-    for i in range(len(t) - 1):
-        tmp.append(t[i] + t[i + 1])
-    tmp.append(1)
-    triangles.append(tmp)
+        for i in range(len(t) - 1):
+            tmp.append(t[i] + t[i + 1])
+        tmp.append(1)
+        triangles.append(tmp)
     return triangles
