7f8a106701ec40447df666001076799e5d3d93ef
Kagendoooo <kagendoooo@gmail.com>
2023-11-14 22:20:32 +0300

    Corrections and all code

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,2 @@
---inserts a new row in the table first_table (database hbtn_0c_0).
-
+-- inserts a new row in the table first_table (database hbtn_0c_0).
 CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
