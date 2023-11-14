37642db3fe2159c45c1d8042a87dfb236fbaec6e
Kagendoooo <kagendoooo@gmail.com>
2023-11-14 22:26:12 +0300

    Fixed code

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
 -- inserts a new row in the table first_table (database hbtn_0c_0).
-CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
