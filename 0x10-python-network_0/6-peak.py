3dc6df820d748c16de47bc622aed2ef0de344c28
Kagendoooo <kagendonikki16@gmail.com>
2024-07-05 00:48:43 +0300

    Find a peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""finds a peak in a list of unsorted integers"""
+
+def find_peak(list_of_integers):
+    """find peak in a list """
+    if not list_of_integers:
+        return None
+
+    i, j = 0, len(list_of_integers) - 1
+
+    while i < j:
+        mid = (i + j) // 2
+
+        if list_of_integers[mid] >= list_of_integers[mid + 1]:
+            j = mid
+        else:
+            i = mid + 1
+
+    return list_of_integers[i]
