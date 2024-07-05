44ede051a18076c8fa2ddaa7383aaa929cad62ff
Kagendoooo <kagendonikki16@gmail.com>
2024-07-06 00:15:56 +0300

    Post an email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,12 +9,12 @@ import sys
 import urllib.request
 import urllib.parse
 
-url = sys.argv[1]
-email = sys.argv[2]
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
 
-data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
 
-with urllib.request.urlopen(url, data) as response:
-    body = response.read()
-
-print(body.decode('utf-8'))
+    with urllib.request.urlopen(url, data) as response:
+        b = response.read()
+        print(b.decode('utf-8'))
