229bac53eb0ee07fc5a1dbec282dfc894776950e
Kagendoooo <kagendonikki16@gmail.com>
2024-08-08 00:16:46 +0300

    Star Wars movies

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,5 +1,5 @@
 //fetches and lists the title for all movies by using a URL
-$.get('https://swapi-api.hbtn.io/api/films/?format=json', function (data) {
+$.get('https://swapi-api.alx-tools.com/api/films/?format=json', function (data) {
   console.log(data);
   data.results.forEach(film => {
     $('UL#list_movies').append('<li>' + film.title + '</li>');
